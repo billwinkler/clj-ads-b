22a16e1c672a657f0c4eb970efe62789e7664dcd
wiseman <jjwiseman@gmail.com>
2015-05-03 13:11:05 -0700

    Added decoding for 4 new messages.
    Added AirbornePositionMsg, AirspeedHeadingMsg, VelocityOverGroundMsg,
    IdentificationMsg.

--- src/clj/com/lemondronor/ads_b.clj
+++ src/clj/com/lemondronor/ads_b.clj
@@ -1,11 +1,153 @@
 (ns com.lemondronor.ads-b
   "Decodes Mode-S messages."
-  (:import (org.opensky.libadsb Decoder)))
+  (:import (org.opensky.libadsb Decoder tools)
+           (org.opensky.libadsb.msgs AirbornePositionMsg AirspeedHeadingMsg
+                                     IdentificationMsg SurfacePositionMsg
+                                     VelocityOverGroundMsg)))
 
 (set! *warn-on-reflection* true)
+
+
+(defprotocol IDictable
+  (as-dict [msg]))
+
+
+(defn hexify [s]
+  (apply str
+         (map #(format "%02x" (int %)) s)))
+
+
+(defmacro assoc-when [test m & args]
+  `(let [m# ~m]
+     (if ~test (assoc m# ~@args) m#)))
+
+
+(extend-type AirbornePositionMsg
+  IDictable
+  (as-dict [msg]
+    (letfn [(add-alt [d]
+              (assoc-when (.hasAltitude msg)
+                d
+                :is-baro-alt (.isBarometricAltitude msg)
+                :alt (.getAltitude msg)))
+            (add-pos [d]
+              (assoc-when (.hasPosition msg)
+                d
+                :cpr-lat (.getCPREncodedLatitude msg)
+                :cpr-lon (.getCPREncodedLongitude msg)
+                :cpr-format (if (.isOddFormat msg) :odd :even)))]
+      (->
+       {:icao (tools/toHexString (.getIcao24 msg))
+        :downlink-format (.getDownlinkFormat msg)
+        :capabilities (.getCapabilities msg)
+        :format-type-code (.getFormatTypeCode msg)
+        :nic-supplement-a (.getNICSupplementA msg)
+        :horizontal-containment-radius-limit (.getHorizontalContainmentRadiusLimit msg)
+        :nic (.getNavigationIntegrityCategory msg)
+        :surveillance-status (.getSurveillanceStatus msg)
+        :surveillance-status-desc (.getSurveillanceStatusDescription msg)
+        :nic-supplement-b (.getNICSupplementB msg)
+        :time-flag (.getTimeFlag msg)}
+       add-alt
+       add-pos))))
+
+
+(extend-type AirspeedHeadingMsg
+  IDictable
+  (as-dict [msg]
+    (letfn [(add-hdg [d]
+              (assoc-when (.hasHeadingInfo msg)
+                d :heading (.getHeading msg)))
+            (add-spd [d]
+              (assoc-when (.hasAirspeedInfo msg)
+                d :airspeed (.getAirspeed msg)))
+            (add-vert-spd [d]
+              (assoc-when (.hasVerticalRateInfo msg)
+                d :vertical-spd (.getVerticalRate msg)))
+            (add-geo-minus-baro [d]
+              (assoc-when (.hasGeoMinusBaroInfo msg)
+                d :geo-minus-baro (.getGeoMinusBaro msg)))]
+      (->
+       {:icao (tools/toHexString (.getIcao24 msg))
+        :downlink-format (.getDownlinkFormat msg)
+        :capabilities (.getCapabilities msg)
+        :format-type-code (.getFormatTypeCode msg)
+        :supersonic? (.isSupersonic msg)
+        :change-intent? (.hasChangeIntent msg)
+        :ifr? (.hasIFRCapability msg)
+        :nac (.getNavigationAccuracyCategory msg)
+        :barometric-vertical-spd? (.isBarometricVerticalSpeed msg)}
+       add-hdg
+       add-spd
+       add-vert-spd
+       add-geo-minus-baro))))
+
+
+(extend-type VelocityOverGroundMsg
+  IDictable
+  (as-dict [msg]
+    (letfn [(add-vel [d]
+              (assoc-when (.hasVelocityInfo msg)
+                d
+                :heading (.getHeading msg)
+                :ground-speed (.getVelocity msg)
+                :e-w-spd (.getEastToWestVelocity msg)
+                :n-s-spd (.getNorthToSouthVelocity msg)))
+            (add-vert-spd [d]
+              (assoc-when (.hasVerticalRateInfo msg)
+                d :vertical-spd (.getVerticalRate msg)))
+            (add-geo-minus-baro [d]
+              (assoc-when (.hasGeoMinusBaroInfo msg)
+                d :geo-minus-baro (.getGeoMinusBaro msg)))]
+      (->
+       {:icao (tools/toHexString (.getIcao24 msg))
+        :downlink-format (.getDownlinkFormat msg)
+        :capabilities (.getCapabilities msg)
+        :format-type-code (.getFormatTypeCode msg)
+        :supersonic? (.isSupersonic msg)
+        :change-intent? (.hasChangeIntent msg)
+        :ifr? (.hasIFRCapability msg)
+        :nac (.getNavigationAccuracyCategory msg)
+        :barometric-vertical-spd? (.isBarometricVerticalSpeed msg)}
+       add-vel
+       add-vert-spd
+       add-geo-minus-baro))))
+
+
+(extend-type IdentificationMsg
+  IDictable
+  (as-dict [msg]
+    {:icao (tools/toHexString (.getIcao24 msg))
+     :downlink-format (.getDownlinkFormat msg)
+     :capabilities (.getCapabilities msg)
+     :format-type-code (.getFormatTypeCode msg)
+     :emitter-category (.getEmitterCategory msg)
+     :category-description (.getCategoryDescription msg)
+     :callsign (String. (.getIdentity msg))}))
+
+
+(extend-type SurfacePositionMsg
+  IDictable
+  (as-dict [msg]
+    (letfn [(add-pos [d]
+              (assoc-when (.hasPosition msg)
+                d :heading (.getHeading msg)))
+            (add-gnd-spd [d]
+              (assoc-when (.hasGroundSpeed msg)
+                d :1))]
+      (->
+       {:icao (tools/toHexString (.getIcao24 msg))
+        :downlink-format (.getDownlinkFormat msg)
+        :capabilities (.getCapabilities msg)
+        :format-type-code (.getFormatTypeCode msg)
+        :nic-supplement (.getNICSupplement msg)
+        :horizontal-containment-radius-limit (.getHorizontalContainmentRadiusLimit msg)
+        :nic (.getNavigationIntegrityCategory msg)}))))
 
 
 (defn decode-hex
   "Decodes a Mode-S message in hex string format."
   [hex-str]
-  (Decoder/genericDecoder hex-str))
+  (-> hex-str
+      (Decoder/genericDecoder)
+      as-dict))
--- test/com/lemondronor/ads_b_test.clj
+++ test/com/lemondronor/ads_b_test.clj
@@ -1,8 +1,57 @@
 (ns com.lemondronor.ads-b-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.pprint :as pprint]
+            [clojure.test :refer :all]
             [com.lemondronor.ads-b :as ads-b]))
 
-(deftest decode-hex-test
-  (testing "Decoding hex string"
-    (let [m (ads-b/decode-hex "8dc0ffee58b986d0b3bd25000000")]
-      (println m))))
+
+(deftest decode-airborne-position-test
+  (testing "Decoding airborne position"
+    (is (= {:horizontal-containment-radius-limit 185.2,
+            :capabilities 0,
+            :nic-supplement-b false,
+            :surveillance-status 0,
+            :alt 10972.800000000001,
+            :is-baro-alt true,
+            :icao "c0ffee",
+            :nic-supplement-a false,
+            :cpr-lon 113957,
+            :cpr-lat 92249,
+            :time-flag false,
+            :nic 8,
+            :surveillance-status-desc "No condition information",
+            :format-type-code 11,
+            :cpr-format :odd,
+            :downlink-format 17}
+         (ads-b/decode-hex "8dc0ffee58b986d0b3bd25000000")))))
+
+
+(deftest decode-velocity-over-ground-test
+  (testing "Decoding velocity over ground"
+    (is (= {:capabilities 0,
+            :e-w-spd -195.48872,
+            :n-s-spd -104.94657600000001,
+            :vertical-spd 0.0,
+            :icao "48cb15",
+            :supersonic? false,
+            :nac 2,
+            :ifr? false,
+            :geo-minus-baro -182.88,
+            :format-type-code 19,
+            :ground-speed 221.87749651860187,
+            :change-intent? false,
+            :barometric-vertical-spd? false,
+            :downlink-format 17,
+            :heading 61.77122381863042}
+           (ads-b/decode-hex "8d48cb1599117d19a00499000000")))))
+
+
+(deftest decode-identification-test
+  (testing "Identification"
+    (is (= {:icao "3c1ff8",
+            :downlink-format 18,
+            :capabilities 4,
+            :format-type-code 2,
+            :emitter-category 0,
+            :callsign "        "
+            :category-description "No ADS-B Emitter Category Information"}
+           (ads-b/decode-hex "903c1ff810820820820820000000")))))

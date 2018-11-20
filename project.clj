68356dc529a95485c5e8ed5e25f11499cf889e27
John Wiseman <jjwiseman@gmail.com>
2018-11-20 00:41:21 -0800

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemondronor/ads-b "0.1.3-SNAPSHOT"
+(defproject com.lemondronor/ads-b "0.1.3"
   :description "Clojure code for parsing ADS-B transponder messages."
   :url "https://github.com/wiseman/clj-ads-b"
   :license {:name "GNU General Public License Version 3"
